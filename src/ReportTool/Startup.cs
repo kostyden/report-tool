4b1d735c87fe5d50743bd4e9db82a66fcb291643
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-05 14:27:55 +0300

    Application's entry point moved to Startup class

---
+++ src/ReportTool/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace ReportTool
+{
+    using System;
+
+    class Startup
+    {
+        [STAThread]
+        static void Main()
+        {
+            var window = new MainWindow();
+
+            var application = new App();
+            application.Run(window);
+        }
+    }
+}
