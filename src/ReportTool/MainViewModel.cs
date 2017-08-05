880063b4906763f270628a3cc4b5871b968268e9
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-05 16:29:11 +0300

    Refactored Columns proerty to be of type ReadOnlyCollection

--- src/ReportTool/MainViewModel.cs
+++ src/ReportTool/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace ReportTool
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.ComponentModel;
     using System.Linq;
+    using System.Runtime.CompilerServices;
     using System.Windows.Input;
 
     public class MainViewModel : INotifyPropertyChanged
@@ -11,7 +13,20 @@
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<string> Columns { get; set; }
+        private ReadOnlyCollection<string> _columns;
+        public ReadOnlyCollection<string> Columns
+        {
+            get
+            {
+                return _columns;
+            }
+
+            private set
+            {
+                _columns = value;
+                RaisePropertyChanged();
+            }
+        }
 
         public ICommand LoadDataCommand { get; }
 
@@ -19,16 +34,20 @@
         {
             _provider = provider;
                 
-            Columns = new ObservableCollection<string>();
+            Columns = new ReadOnlyCollection<string>(new List<string>());
             LoadDataCommand = new Command(param => LoadData((string)param));
         }
 
         private void LoadData(string path)
         {
             var data = _provider.GetFrom(path);
-            var uniqueColumns = data.SelectMany(row => row.Keys).Distinct();
-            Columns = new ObservableCollection<string>(uniqueColumns);
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Columns)));
+            var uniqueColumns = data.SelectMany(row => row.Keys).Distinct().ToList();
+            Columns = new ReadOnlyCollection<string>(uniqueColumns);
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
