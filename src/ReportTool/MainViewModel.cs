341b48b1d90f0cdbdc9ff03653e05e1cef35751c
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-05 14:36:43 +0300

    Introduced MainViewModel

---
+++ src/ReportTool/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ReportTool
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    public class MainViewModel
+    {
+        public ObservableCollection<string> Columns { get; set; }
+
+        public MainViewModel()
+        {
+            Columns = new ObservableCollection<string>();
+        }
+    }
+}
---
+++ tests/ReportTool.Tests/MainViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿namespace ReportTool.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    class MainViewModelTests
+    {
+        [Test]
+        public void Columns_ShouldBeEmptyAfterInitialization()
+        {
+            var viewmodel = new MainViewModel();
+            viewmodel.Columns.Should().BeEmpty();
+        }
+    }
+}
