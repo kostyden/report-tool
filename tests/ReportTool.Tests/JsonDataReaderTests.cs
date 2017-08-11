e1f398c28bd4b47d1bb0f113b0aa4b35690dfe85
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-11 13:16:30 +0300

    Fixed testcase data

--- tests/ReportTool.Tests/JsonDataReaderTests.cs
+++ tests/ReportTool.Tests/JsonDataReaderTests.cs
@@ -28,7 +28,7 @@
                 new Dictionary<string, double> {{ "weight", 148.57 }, { "speed", 221.99 }, { "tyre-consumption", 0.9212 } },
                 new Dictionary<string, double> {{ "weight", 146.98 }, { "speed", 225.05 }, { "tyre-consumption", 0.8601 } },
                 new Dictionary<string, double> {{ "weight", 144.00 }, { "speed", 228.45 }, { "tyre-consumption", 0.7915 } },
-                new Dictionary<string, double> {{ "weight", 143.05 }, { "speed", 120.98 }, { "tyre-consumption", 0.7801 } }
+                new Dictionary<string, double> {{ "weight", 142.00 }, { "speed", 229.98 }, { "tyre-consumption", 0.7000 } }
             };
             var reader = new JsonDataReader();
 
