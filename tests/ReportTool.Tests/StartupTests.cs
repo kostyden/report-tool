46030c075fbeccdcbdd2ede8f7d18fab159cecd5
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-11 20:50:10 +0300

    Restored Startup tests

---
+++ tests/ReportTool.Tests/StartupTests.cs
@@ -0,0 +1,53 @@
+﻿namespace ReportTool.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System.Linq;
+    using TestStack.White;
+    using TestStack.White.UIItems;
+    using TestStack.White.UIItems.Finders;
+
+    [TestFixture]
+    class StartupTests
+    {
+        [Test]
+        public void ShouldOpenMainWindowWithDefaultView()
+        {
+            using (var application = LaunchApplication())
+            {
+                var window = application.GetWindows().First();
+                var textBox = window.Get<TextBox>(SearchCriteria.ByAutomationId("filePath"));
+                textBox.Text.Should().BeEmpty();
+            }
+        }
+
+        [Test]
+        [TestCase(@"DataReadersTestCases\ExcelDataReaderTestCase1.xlsx", new[] { "one", "two", "three" })]
+        [TestCase(@"DataReadersTestCases\CsvDataReaderTestCase1CommaDelimited.csv", new[] { "one", "two", "three" })]
+        [TestCase(@"DataReadersTestCases\JsonDataReaderTestCase1.json", new[] { "weight", "speed", "tyre-consumption" })]
+        public void ShouldDisplayCorrectColumnsAfterLoadingData(string fileName, string[] expectedColumnNames)
+        {
+            using (var application = LaunchApplication())
+            {
+                var window = application.GetWindows().First();
+                var textBox = window.Get<TextBox>(SearchCriteria.ByAutomationId("filePath"));
+                var loadButton = window.Get<Button>(SearchCriteria.ByAutomationId("loadData"));
+
+                textBox.BulkText = TestUtils.BuildPathFor(fileName);
+                loadButton.Click();
+
+                var actualColumnNames = window.GetMultiple(SearchCriteria.ByAutomationId("columnName"))
+                                              .OfType<WPFLabel>()
+                                              .Select(label => label.Text);
+
+                actualColumnNames.ShouldBeEquivalentTo(expectedColumnNames);
+            }
+        }
+
+        private Application LaunchApplication()
+        {
+            var exePath = TestUtils.BuildPathFor(@"ReportTool.exe");
+            return Application.Launch(exePath);
+        }
+    }
+}
